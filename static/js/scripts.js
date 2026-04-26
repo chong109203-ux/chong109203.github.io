4fcad56ebf24016f52fff609b20c9f4a9dca581e
Yichong Dong <chong109203@gmail.com>
2026-04-26 16:52:15 +0800

    Translate Chinese publication titles into English

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
 const section_names = ['home', 'experience', 'publications', 'awards']
-const cache_buster = '20260425-6'
+const cache_buster = '20260426-1'
 
 
 window.addEventListener('DOMContentLoaded', event => {

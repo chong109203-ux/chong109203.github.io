865d53ff261a8e343d2582b9e2c103b9eb5f3418
Yichong Dong <chong109203@gmail.com>
2026-04-26 16:56:57 +0800

    Rename awards section to awards and service

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
 const section_names = ['home', 'experience', 'publications', 'awards']
-const cache_buster = '20260426-3'
+const cache_buster = '20260426-4'
 
 
 window.addEventListener('DOMContentLoaded', event => {
